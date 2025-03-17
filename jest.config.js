c70adc6532f9dea15c00dc17b50a592bcebcd61d
Eugeniya Volegzhanina <volegzhaninaem@yandex.ru>
2025-03-18 01:15:17 +0700

    Настроила пре-коммит хуки и окружение тестов

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// jest.config.js
+export default {
+  preset: 'ts-jest', // Используем ts-jest для TypeScript
+  testEnvironment: 'jsdom', // Для тестирования DOM-элементов
+  setupFilesAfterEnv: ['./jest.setup.ts'], // Настройка глобальных переменных или утилит
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // Поддержка псевдонимов
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy' // Игнорируем стили
+  },
+  testMatch: ['**/tests/**/*.test.[jt]s?(x)'] // Где искать тесты
+}
---
+++ tests/router/router.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from '@jest/globals'
+
+describe('Router', () => {
+
+  it('should add a route with use()', () => {
+    expect(1).toBe(1)
+  })
+})
